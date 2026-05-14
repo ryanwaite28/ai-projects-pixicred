f7b48bd30c01c9b9f5280773de2fe29504e2afba
Ryan Waite <ryanwaite28@gmail.com>
2026-05-14 16:34:01 -0400

    fixed lint and typecheck errors

--- tests/integration.setup.ts
+++ tests/integration.setup.ts
@@ -1,6 +1,12 @@
 import { inject } from 'vitest';
 
-const url = inject('testDatabaseUrl') as string | undefined;
+declare module 'vitest' {
+  export interface ProvidedContext {
+    testDatabaseUrl: string;
+  }
+}
+
+const url = inject('testDatabaseUrl');
 if (url) {
   process.env['TEST_DATABASE_URL'] = url;
   process.env['DATABASE_URL'] = url;
